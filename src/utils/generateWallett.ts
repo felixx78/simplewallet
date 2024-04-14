4b17378cf466e519d0d7841af3df4daab9526971
v1st78 <petrenkov605@gmail.com>
2024-04-14 23:13:33 +0300

    add generateWallet

---
+++ src/utils/generateWallett.ts
@@ -0,0 +1,36 @@
+import * as bip39 from "bip39";
+import BIP32Factory from "bip32";
+import * as ecc from "tiny-secp256k1";
+import { keccak_256 } from "@noble/hashes/sha3";
+import { Buffer } from "buffer";
+import { ec as EC } from "elliptic";
+
+const ec = new EC("secp256k1");
+
+const bip32 = BIP32Factory(ecc);
+
+function generateWallet(seedPhrase: string, derivePath: string) {
+  const seed = bip39.mnemonicToSeedSync(seedPhrase);
+
+  const node = bip32.fromSeed(seed);
+
+  const childNode = node.derivePath(derivePath);
+
+  const privateKey = "0x" + childNode.privateKey!.toString("hex");
+  const publicKey = "0x" + childNode.publicKey!.toString("hex");
+
+  const ecKey = ec.keyFromPublic(childNode.publicKey, "hex");
+  const uncompressPublicKey = ecKey.getPublic().encode("hex", false).slice(2);
+  const adressUInt = keccak_256(Buffer.from(uncompressPublicKey, "hex")).slice(
+    -20
+  );
+  const adress = "0x" + Buffer.from(adressUInt).toString("hex");
+
+  return {
+    adress: adress,
+    publicKey,
+    privateKey,
+  };
+}
+
+export default generateWallet;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [wasm(), topLevelAwait(), react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+});
