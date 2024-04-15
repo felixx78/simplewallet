1e92836b3bb26c9cf909e37e9bd7a23c49c1cb53
v1st78 <petrenkov605@gmail.com>
2024-04-15 23:01:17 +0300

    add icons to cryptos

--- src/utils/cryptos.ts
+++ src/utils/cryptos.ts
@@ -3,6 +3,7 @@ export type CryptoType = {
   contactAddress: string;
   network: string;
   tokenStandart: string;
+  icon: string;
 };
 
 const cryptos: CryptoType[] = [
@@ -11,12 +12,14 @@ const cryptos: CryptoType[] = [
     contactAddress: "0x55d398326f99059ff775485246999027b3197955",
     network: "BNB Chain",
     tokenStandart: "bep-20",
+    icon: "/usdt.svg",
   },
   {
     name: "usdt",
     contactAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
     network: "Etherium Mainnet",
     tokenStandart: "erc-20",
+    icon: "/usdt.svg",
   },
 ];
 
