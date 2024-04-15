d5aeacbfb6be884a5adfff7b21209188764403e6
v1st78 <petrenkov605@gmail.com>
2024-04-15 22:53:53 +0300

    add tokenStandart to cryptos

--- src/utils/cryptos.ts
+++ src/utils/cryptos.ts
@@ -2,6 +2,7 @@ export type CryptoType = {
   name: string;
   contactAddress: string;
   network: string;
+  tokenStandart: string;
 };
 
 const cryptos: CryptoType[] = [
@@ -9,11 +10,13 @@ const cryptos: CryptoType[] = [
     name: "usdt",
     contactAddress: "0x55d398326f99059ff775485246999027b3197955",
     network: "BNB Chain",
+    tokenStandart: "bep-20",
   },
   {
     name: "usdt",
     contactAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
     network: "Etherium Mainnet",
+    tokenStandart: "erc-20",
   },
 ];
 
