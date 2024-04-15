e20412787baa781537d8a201a000a6ac102751e3
v1st78 <petrenkov605@gmail.com>
2024-04-15 23:17:26 +0300

    add network icons

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -2,6 +2,7 @@ export type Network = {
   name: string;
   derivePath: string;
   web3: string;
+  icon: string;
 };
 
 const networks: Array<Network> = [
@@ -9,11 +10,13 @@ const networks: Array<Network> = [
     name: "Etherium Mainnet",
     derivePath: "m/44'/60'/0'/0/0",
     web3: "https://rpc.ankr.com/eth",
+    icon: "/eth.jpg",
   },
   {
     name: "BNB Chain",
     derivePath: "m/44'/60'/0'/0/0",
     web3: "https://bsc-dataseed1.binance.org:443",
+    icon: "/bnb.svg",
   },
 ];
 
