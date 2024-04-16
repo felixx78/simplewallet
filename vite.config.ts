80b864f8e91ba36e7bd405662b90c0c34af0fda0
v1st78 <petrenkov605@gmail.com>
2024-04-16 20:35:46 +0300

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,11 +22,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
+      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
     },
   },
 });
