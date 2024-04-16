cf5560b6f50ad5343917c38dfeee16afe3f1fba3
v1st78 <petrenkov605@gmail.com>
2024-04-16 23:32:56 +0300

    fix small screen step 1 CreateAndImport

--- src/pages/CreateAndImport.tsx
+++ src/pages/CreateAndImport.tsx
@@ -15,7 +15,7 @@ function CreateAndImport({ create = false }: { create?: boolean }) {
 
   return (
     <div
-      className={`flex items-center justify-center sm:h-[80vh] flex-col gap-6 ${steps === 1 ? "h-[100vh]" : "h-[80vh]"}`}
+      className={`flex items-center justify-center sm:h-[80vh] flex-col gap-6 ${steps === 1 ? "h-[100vh] min-h-[1000px] sm:min-h-0" : "h-[80vh]"}`}
     >
       <Logo center />
       <div className="border-border border-2 px-8 pt-6 pb-6 rounded-md min-w-[300px] max-w-[450px]">
