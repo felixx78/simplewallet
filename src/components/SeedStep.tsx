4b00653569c213fa705706b6a4f411134495635d
v1st78 <petrenkov605@gmail.com>
2024-04-16 19:46:44 +0300

    add SeedStep

---
+++ src/components/SeedStep.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { useSeed } from "../contexts/SeedContext";
+
+type Props = {
+  onBack: () => void;
+  onContinue: () => void;
+};
+
+function SeedStep({ onBack, onContinue }: Props) {
+  const seed = useSeed();
+
+  const [inputData, setInputData] = useState(
+    Array.from({ length: 12 }, () => "")
+  );
+
+  const handleOnChange = (v: string, index: number) => {
+    const newInputData = [...inputData];
+    newInputData[index] = v;
+
+    setInputData(newInputData);
+  };
+
+  const handleOnPaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    const pasted = e.clipboardData.getData("text");
+    const pastedSplit = pasted.trim().split(" ");
+
+    if (pastedSplit.length === 12) {
+      e.preventDefault();
+      setInputData(pastedSplit);
+    }
+  };
+
+  const handleOnContinue = () => {
+    seed.setSeed(inputData.map((i) => i.trim()).join(" "));
+    onContinue();
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl mb-6 text-center">Enter your 12 words</h1>
+      <div className="grid grid-cols-2 gap-4 mb-6">
+        {inputData.map((value, index) => (
+          <div key={index} className="flex gap-2 items-center">
+            <input
+              onPaste={handleOnPaste}
+              className="text-copy bg-foreground px-4 py-2 w-[150px] outline-none"
+              onChange={(e) => handleOnChange(e.target.value, index)}
+              placeholder={`word #${index + 1}`}
+              value={value}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-4 justify-between">
+        <button
+          onClick={onBack}
+          className="block w-1/2 border-border border-2 py-2 rounded-md"
+        >
+          Back
+        </button>
+        <button
+          onClick={handleOnContinue}
+          disabled={inputData.findIndex((i) => i === "") !== -1}
+          className="block w-1/2 bg-border rounded-md py-2 disabled:bg-foreground"
+        >
+          Continue
+        </button>
+      </div>
+    </div>
+  );
+}
+export default SeedStep;
