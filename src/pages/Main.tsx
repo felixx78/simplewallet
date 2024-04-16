434614093017cd8ecaf57ddbd7c019a1ef5e989f
v1st78 <petrenkov605@gmail.com>
2024-04-16 18:05:43 +0300

    add Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,32 @@
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import cryptos from "../utils/cryptos";
+import CryptoItem from "../components/CryptoItem";
+import getNetworks, { Network } from "../utils/networks";
+import { useSeed } from "../contexts/SeedContext";
 
 function Main() {
-  return <NavLink to="/test">test</NavLink>;
+  const seed = useSeed();
+
+  const [networks, setNetworks] = useState<Network[] | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      const newNetworks = await getNetworks(seed.seed);
+      setNetworks(newNetworks);
+    })();
+  }, [seed.seed]);
+
+  return (
+    <div className="flex flex-col gap-6 px-6 py-2">
+      {networks &&
+        cryptos.map((i) => (
+          <CryptoItem
+            key={i.name + i.network}
+            data={i}
+            network={networks.find((n) => n.name === i.network)!}
+          />
+        ))}
+    </div>
+  );
 }
 export default Main;
