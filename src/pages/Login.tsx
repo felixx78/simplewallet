3f527ad303854951844428e4991f3721b64d3911
v1st78 <petrenkov605@gmail.com>
2024-04-16 20:15:44 +0300

    update Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,7 +12,9 @@ function Login() {
 
   const seed = useSeed();
 
-  const handleUnlock = () => {
+  const handleUnlock = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     if (seed.auth(password)) {
       navigate("/");
       return;
@@ -28,22 +30,24 @@ function Login() {
       <div className="border-border border-2 px-8 pt-6 pb-8 rounded-md max-w-[450px]">
         <h1 className="text-2xl mb-6 text-center">Enter password</h1>
 
-        <PasswordInput
-          placeholder="Password"
-          value={password}
-          error={error}
-          onChange={(v) => setPassword(v)}
-        />
+        <form onSubmit={handleUnlock}>
+          <PasswordInput
+            placeholder="Password"
+            value={password}
+            error={error}
+            onChange={(v) => setPassword(v)}
+          />
 
-        <div className="mb-6"></div>
+          <div className="mb-6"></div>
 
-        <button
-          onClick={handleUnlock}
-          disabled={!password}
-          className="block w-full bg-border rounded-md py-2 disabled:bg-foreground"
-        >
-          Unlock
-        </button>
+          <button
+            type="submit"
+            disabled={!password}
+            className="block w-full bg-border rounded-md py-2 disabled:bg-foreground"
+          >
+            Unlock
+          </button>
+        </form>
       </div>
     </div>
   );
