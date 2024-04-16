c1fd438ddadd70e32f3ba9325d6967af4bc66f27
v1st78 <petrenkov605@gmail.com>
2024-04-16 17:54:37 +0300

    add utils/seed

---
+++ src/utils/seed.ts
@@ -0,0 +1,17 @@
+import { AES, enc } from "crypto-js";
+
+export function encryptSeedPhrase(seed: string, password: string) {
+  const encryptedSeed = AES.encrypt(seed, password).toString();
+
+  return encryptedSeed;
+}
+
+export function decryptSeedPhrase(password: string) {
+  const encryptedSeed = localStorage.getItem("seed");
+  if (!encryptedSeed) return null;
+
+  const decryptedBytes = AES.decrypt(encryptedSeed, password);
+  const decryptedSeed = decryptedBytes.toString(enc.Utf8);
+
+  return decryptedSeed;
+}
