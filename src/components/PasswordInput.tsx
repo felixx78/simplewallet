cb3537f503372a4d9df7fade7943b03456517aa6
v1st78 <petrenkov605@gmail.com>
2024-04-16 19:27:46 +0300

    add PasswordInput

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { LuEye, LuEyeOff } from "react-icons/lu";
+
+type Props = {
+  value: string;
+  onChange: (v: string) => void;
+  placeholder?: string;
+};
+
+function PasswordInput({ value, onChange, placeholder }: Props) {
+  const [isHidden, setIsHidden] = useState(true);
+
+  return (
+    <div className="relative">
+      <input
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        className="text-copy bg-foreground pl-4 pr-14 py-2 w-full outline-none"
+        type={isHidden ? "password" : "text"}
+      />
+      <button
+        onClick={() => setIsHidden(!isHidden)}
+        className="absolute right-4 top-1/2 -translate-y-1/2"
+      >
+        {isHidden ? <LuEye size="20px" /> : <LuEyeOff size="20px" />}
+      </button>
+    </div>
+  );
+}
+export default PasswordInput;
