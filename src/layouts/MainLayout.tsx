b26a6ca631198dfc943a4c4d5d57c450322d3d7c
v1st78 <petrenkov605@gmail.com>
2024-04-17 18:46:08 +0300

    update MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <main
+    <div
       style={{ fontFamily: "'Inter', sans-serif" }}
       className="bg-background min-h-screen text-copy-light px-3 sm:px-4 md:px-0"
     >
       <div className="max-w-[450px] mx-auto">
         <Outlet />
       </div>
-    </main>
+    </div>
   );
 }
 export default MainLayout;
