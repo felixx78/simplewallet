60125839d9ac193c411af470173c0570c4ff058f
v1st78 <petrenkov605@gmail.com>
2024-04-17 19:19:20 +0300

    remove unused import RequireLog

--- src/components/RequireLog.tsx
+++ src/components/RequireLog.tsx
@@ -1,7 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useWallet } from "../contexts/WalletContext";
 import Spinner from "./Spinner";
-import Logo from "./Logo";
 
 function RequireLog({ children }: { children: React.ReactNode }) {
   const wallet = useWallet();
