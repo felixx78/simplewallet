70d75aeacca98c70b488ac8cb0ee0776c8165e57
v1st78 <petrenkov605@gmail.com>
2024-04-17 14:27:42 +0300

    update MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,23 +1,6 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useSeed } from "../contexts/SeedContext";
+import { Outlet } from "react-router-dom";
 
 function MainLayout() {
-  const seed = useSeed();
-  const location = useLocation();
-
-  const ignorePaths = [
-    "/welcome",
-    "/wallet/import",
-    "/wallet/create",
-    "/login",
-  ];
-  if (!seed.seed && !ignorePaths.includes(location.pathname)) {
-    if (localStorage.getItem("seed") && localStorage.getItem("password")) {
-      return <Navigate to="/login" />;
-    }
-    return <Navigate to="/welcome" />;
-  }
-
   return (
     <main
       style={{ fontFamily: "'Inter', sans-serif" }}
