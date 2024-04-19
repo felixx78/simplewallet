ea04bdb462c429c4eaf6ded8572e5f95e3988bf2
v1st78 <petrenkov605@gmail.com>
2024-04-19 23:57:53 +0300

    update size of sm Spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 function Spinner({ size = "sm" }: { size?: "sm" | "lg" }) {
   return (
     <div
-      className={`${size === "sm" ? "h-8 w-8" : "h-16 w-16"} inline-block animate-spin rounded-full border-copy-light border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
+      className={`${size === "sm" ? "h-6 w-6" : "h-16 w-16"} inline-block animate-spin rounded-full border-copy-light border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
